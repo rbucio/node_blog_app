a2e454662d9cbdc79cbfbdfa2fd9e3865e63c9d5
Rey <rbuciodev@gmail.com>
2017-10-14 00:47:35 -0700

    install express ejs body-parser mongoose, created public, routes and views folders

--- server.js
+++ server.js
@@ -1,5 +1,39 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongoose = require('mongoose');
+
+
+// CONNECT TO DATABASE
+// mongoose.connect('mongodb://localhost/blogApp', (err) => {
+//     if (err) return console.log(err);
+//     console.log('CONNECTION TO DB SUCCESFULL!');
+// })
+
+// INITIALIZE APP
 const app = express();
+
+// SET VIEW ENGINE
+app.set('views', path.join(__dirname + '/views'));
+app.set('view engine', 'ejs');
+
+// SET PATH TO PUBLIC FOLDER
+app.use(express.static(path.join(__dirname + 'public')));
+
+
+//
+// MIDDLEWARE
+//
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+//
+// ROUTES
+//
+const appRoutes = require('./routes/app.js');
+
+app.use('/', appRoutes);
 
 app.listen(3000, () => {
     console.log('SERVER IS RUNNING!')
