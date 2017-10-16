d09482abbe5c1a947b5e6882a56717866291382a
Rey <rbuciodev@gmail.com>
2017-10-15 21:52:25 -0700

    added single post route and page template

--- routes/post.js
+++ routes/post.js
@@ -4,74 +4,79 @@ const router = express.Router();
 // GET BLOG MODEL
 const Post = require('../models/post');
 
+//
+// GET ALL POST
+//
 router.get('/', (req, res) => {
 
-    //GET ALL POSTS
     Post.find({}, (err, posts) => {
         if (err) console.log(err);
         res.render('posts', { pageTitle: 'All Post', posts: posts });
     });
-});
 
+});
+//
+// SHOW SINGLE POST
+//
+router.get('/:id', (req, res) => {
+
+    Post.findOne({_id: req.params.id}, (err, post) => {
+
+        if (err) console.log(err)
+
+        res.render('singlePost', { pageTitle: post.title, post: post });
+    });
+
+});
+//
+// CREATE BLOG POST
+//
 router.post('/', (req, res) => {
 
-    // // FORMAT DATE TO USE TIME AGO FORMAT
-    // var date = '';
-    // var day = new Date().getDate().toString();
-    // var month = new Date().getMonth();
-    //     month += 1;
-    //     month = month.toString();
-    // var year = new Date().getFullYear().toString();
-    //
-    // // ADD CERO TO SINGLE DIGIT DAYS
-    // if (day.length < 2) {
-    //     day = '0' + day;
-    // }
-    // // ADD CERO TO SINGLE DIGIT MONTH
-    // if (month.length < 2) {
-    //     month = '0' + month;
-    // }
-    //
-    // date = year + month + day;
-    //
-    //
-    // // CREATE POST
-    // const post = new Post({
-    //     title: req.body.postTitle,
-    //     author: req.body.postAuthor,
-    //     image: req.body.postImage,
-    //     content: req.body.postContent,
-    //     datePublished: date
-    // });
-    //
-    // //SAVE POST
-    // post.save( (err) => {
-    //     if (err) console.log(err);
-    //     res.redirect('/');
-    // });
+    // FORMAT DATE TO USE TIME AGO FORMAT
+    var date = '';
+    var day = new Date().getDate().toString();
+    var month = new Date().getMonth();
+        month += 1;
+        month = month.toString();
+    var year = new Date().getFullYear().toString();
 
-    var date = new Date();
-    var utfOffset = date.getTimezoneOffset();
-    var localTime = date.getTime();
-    var localOffset = utfOffset * 60000;
-    var offset = -8;
-    var utc = localTime + localOffset;
-    var pacificTime = utc + (3600000 * offset);
-    var newDate = new Date(pacificTime);
+    // ADD CERO TO SINGLE DIGIT DAYS
+    if (day.length < 2) {
+        day = '0' + day;
+    }
+    // ADD CERO TO SINGLE DIGIT MONTH
+    if (month.length < 2) {
+        month = '0' + month;
+    }
 
-    var time = 420 * 60000;
+    date = year + month + day;
 
-    console.log('420 * 60000 is ' + time)
-    console.log('Date is ' + date);
-    console.log('Time offset is ' + utfOffset);
-    console.log('The local offset time is ' + localOffset);
-    console.log(newDate);
 
-    res.redirect('/');
+    // CREATE POST
+    const post = new Post({
+        title: req.body.postTitle,
+        author: req.body.postAuthor,
+        image: req.body.postImage,
+        content: req.body.postContent,
+        datePublished: date
+    });
+
+    //SAVE POST
+    post.save( (err) => {
+        if (err) console.log(err);
+        res.redirect('/');
+    });
+
 });
 
+//
+// ADD NEW POST
+//
 router.get('/new', (req, res) => {
+
     res.render('postNew', { pageTitle: 'Create Post' });
+
 });
 
 module.exports = router;
